fcc1f4c2c648f41f11848101a5c8bbf08c2b6280
Ruben Samaey <ruben.samaey@gmail.com>
2014-11-23 21:40:18 +0100

    ui was updated

--- course_proj_dev_data_prod/app/ui.R
+++ course_proj_dev_data_prod/app/ui.R
@@ -4,19 +4,19 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Prediction of birthweight based on situation of the Mother."),
+  titlePanel("Prediction of birthweight based on the condition of the mother."),
   
-  h6("The data used in this application was sourced from:"),
-  p("Hosmer, D.W., Lemeshow, S. and Sturdivant, R.X. (2013); Applied Logistic Regression: Third Edition."),
-  p("These data are copyrighted by John Wiley & Sons Inc. and must 
-  be acknowledged and used accordingly."),
-  p("Data were collected at Baystate Medical Center, Springfield, Massachusetts during 1986."),
-  br(),br(),
- 
+  # Explanation
+  h4("How to use this application"),
+  p("This application aims to predict the birthweight of a newborn based on the condition of the mother. 
+  By specifying the condition of the pregnant mother an estimation of the expected birthweight is given."),
+  p("Note: a model is built when starting this application, this takes some time.
+  See at the bottom of the page for a reference to the data used for building the model."),
+  
   
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
-    sidebarPanel(h4("Situation of Mother"),
+    sidebarPanel(h4("Situation of the mother"),
       sliderInput("Age",
                   "Age of Mother: [Years]",
                   min = 14,
@@ -46,17 +46,25 @@ shinyUI(fluidPage(
     # Show a plot of the generated distribution
     mainPanel(
       h2("Birthweight Prediction"),
+      p("Note: a model is built when starting this application, this takes some time. When the model is ready an estimation will appear based on the inputs on the left."),
       wellPanel(
-        p("Note: a model is built when starting this application, this takes some time.")
-      ),
       h5("Expected birthweigth"),
       textOutput("BirthWeight"),
+      br(),br(),
+      p("Standard Deviation: +/- 700 Grams")
+      ),    
       br(),
-      p("Standard Deviation: +/- 700 Grams"),
-      wellPanel(
-        p("Note: The model has quite a large uncertainty."),
-        p("Probably more data and a better model is needed."),
-        p("However this is beyond the scope of the \"Developming Data Products\" assignment which focusses on building a Shiny application.")    
-    )
+      p("Note: The model has quite a large uncertainty. Probably more data and a better model is needed.
+      However this is beyond the scope of the \"Developming Data Products\" assignment which focusses on building a Shiny application.")    
+      
+    ) # end of mainPanel
+  ), # end of SidebarLayout
+  
+  wellPanel(
+    h6("The data used in this application was sourced from:"),
+    p("Hosmer, D.W., Lemeshow, S. and Sturdivant, R.X. (2013); Applied Logistic Regression: Third Edition."),
+    p("These data are copyrighted by John Wiley & Sons Inc. and must be acknowledged and used accordingly."),
+    p("Data were collected at Baystate Medical Center, Springfield, Massachusetts during 1986.")
   )
-)))
+  ) #end of FluidPage
+) #end of shinyUI
